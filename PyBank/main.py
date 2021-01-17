a6d63a0a81121c573a01a07508189b1bb845a29b
emedina2 <74868338+emedina2@users.noreply.github.com>
2021-01-17 14:49:39 -0600

    Update to PyBank Main file

--- PyBank/main.py
+++ PyBank/main.py
@@ -9,27 +9,29 @@ with open(budgetdata, 'r') as csvfile:
     budgetdata_csv = csv.reader(csvfile, delimiter=',')
     #print(budgetdata_csv)
     ##print(type(budgetdata_csv))
-for row in budgetdata_csv:
-#read header row
-  csv_header = next(budgetdata_csv)
-  print(f"Header:{csv_header}")
 
+    #read header row
+    csv_header = next(budgetdata_csv)
+    print(f"Header:{csv_header}")
+    
+    for row in budgetdata_csv:
     #create variables for data
-  dates = str(budgetdata_csv[0])
-  print(dates)
-#count dates
-    #totalmonths = len(dates)
+      dates = str(row[0])
+      print(dates)
+      #count dates
+      totalmonths = len(dates)
+      print(totalmonths)
 
-#display total months
+    #display total months
     #print(totalmonths)
 
-  #* The net total amount of "Profit/Losses" over the entire period
-#Total Sum of P&L
+    #* The net total amount of "Profit/Losses" over the entire period
+    #Total Sum of P&L
 
     ##TotalPNL = sum(int(budgetdata_csv[1]))
     ##print(TotalPNL)
-  #* Calculate the changes in "Profit/Losses" over the entire period, then find the average of those changes
+    #* Calculate the changes in "Profit/Losses" over the entire period, then find the average of those changes
     ##Changes between each 
-  #* The greatest increase in profits (date and amount) over the entire period
+    #* The greatest increase in profits (date and amount) over the entire period
 
-  #* The greatest decrease in losses (date and amount) over the entire period
+    #* The greatest decrease in losses (date and amount) over the entire period
