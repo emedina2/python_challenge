3bf0f0c05c6623479c08934b7bf6e9ec5ddf0c24
emedina2 <74868338+emedina2@users.noreply.github.com>
2021-01-20 00:17:00 -0600

    Update main.py

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -4,7 +4,6 @@ import os
 import numpy as np
 
 #Create lists for data
-voter = []
 candidates = []
 candidate = []
 candidatevotes= [] 
@@ -14,31 +13,55 @@ with open(os.path.join("Resources","election_data.csv"))as v:
   csv_header = next(poll_data)
 
   for row in poll_data:
-    voter.append(row[0])
     candidate.append(row[2])
 
 #total number of votes
-total_votes = int(len(voter))
-print(f"The total votes is {total_votes}")
+total_votes = int(len(candidate))
 
-  #All candidates
+#Find All candidates
 candidates = np.unique(candidate)
-print(f"The Candidates are {candidates}")
-  #define function to count votes
+
+#define function to count votes
 def countvotes(x):
     return candidate.count(x)
-  #candidate percentages & votes
+
+#Use function to find candidate percentages & votes
 for x in range(len(candidates)):
     cv = countvotes(candidates[x])
     candidatevotes.append(cv)
     percent = (cv/total_votes) * 100
-    votepercent.append(percent)
+    votepercent.append(round(percent,3))
 
-print(candidatevotes)
-print(votepercent)
+candidate_data = {}
+for x in range(len(candidates)):
+  candidate_data = {"Candidate": candidates[x], "Votes": candidatevotes[x], "Percent": votepercent[x]}
 
-  #winner based on popular vote
+#winner based on popular vote
+maxvote = max(candidatevotes)
+index = candidatevotes.index(maxvote)
+winner = candidates[index]
+
 
   #print results
+print(f"""
+  --------------------
+  Election Results
+  --------------------
+  Total Votes: {total_votes}
+  ---------------------
+""")
 
+for row in candidate_data:
+  #print(candidate_data)
+  print(f"{candidate_data['Candidate']} : {candidate_data['Percent']} % ({candidate_data['Votes']}) ")
+
+
+
+print(f'''
+--------------------
+Winner: {winner}
+--------------------
+''')
   #output results to csv
+
+  #["Candidate", "Votes for Candidate", "Percent of Vote"]
