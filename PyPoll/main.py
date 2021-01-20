0fed88b6689131c83768390b535cde04b92e03b7
emedina2 <74868338+emedina2@users.noreply.github.com>
2021-01-19 21:46:54 -0600

    Update main.py
    Added candidate votes & percents.

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -8,7 +8,7 @@ voter = []
 candidates = []
 candidate = []
 candidatevotes= [] 
-
+votepercent = []
 with open(os.path.join("Resources","election_data.csv"))as v:
   poll_data = csv.reader(v, delimiter=",")
   csv_header = next(poll_data)
@@ -24,14 +24,19 @@ print(f"The total votes is {total_votes}")
   #All candidates
 candidates = np.unique(candidate)
 print(f"The Candidates are {candidates}")
-
-
+  #define function to count votes
+def countvotes(x):
+    return candidate.count(x)
   #candidate percentages & votes
 for x in range(len(candidates)):
-    if candidates == candidate:
-        candidatevotes[x] += 1
+    cv = countvotes(candidates[x])
+    candidatevotes.append(cv)
+    percent = (cv/total_votes) * 100
+    votepercent.append(percent)
 
 print(candidatevotes)
+print(votepercent)
+
   #winner based on popular vote
 
   #print results
